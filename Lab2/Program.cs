9162829c1264ea6202d187a2eaea1f42f3989d45
Ekaterina Shvech <mad-et@yandex.ru>
2021-08-08 20:55:21 +0300

    Добавьте файлы проекта.

---
+++ Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        // Метод находит сумму двух чисел
+        static void Main(string[] args)
+        {
+            // Арифметические операции: + , - , * , / , %
+            // Математические функции: Abs, Pow, Sqrt, Cbrt, Min, Max, Round, Sign, PI
+            // Инкремент, декремент: ++, --
+            // Операции со ...: += , -= , *= , /= , %=
+            int a = 1000;
+            byte b = (byte) a;
+            Console.WriteLine(b);
+            Console.Write("Для завершения нажмите любую клавишу на клавиатуре");
+            Console.ReadKey();
+            
+        }
+    }
+}
